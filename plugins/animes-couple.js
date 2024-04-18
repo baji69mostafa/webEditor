77c47a3accc66a05f47df417b7c001b9494c8bab
Shinichi <133603446+maykel2007@users.noreply.github.com>
2024-04-18 19:03:44 +0300

    〘𝐶𝛩𝑁𝐴𝑁╎👑╎𝐵𝛩𝑇〙

--- plugins/animes-couple.js
+++ plugins/animes-couple.js
@@ -5,13 +5,13 @@ let handler = async (m, { conn }) => {
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \nولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_\n انضم الى قناة المطور \nhttps://whatsapp.com/channel/0029VaU3KnKGehELDhZMcf3d', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n ولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n انضم الى قناة المطور \nhttps://whatsapp.com/channel/0029VaU3KnKGehELDhZMcf3d', m)
 }
 handler.help = ['ppcouple','tofanime']
 handler.tags = ['anime']
-handler.command = ['ppcouple','tofanime'] 
+handler.command = ['طقم2','تطقيم2'] 
 
 
 export default handler
