da62298d0fc24906014fc4d0547374155eb47f11
Shinichi <133603446+maykel2007@users.noreply.github.com>
2024-04-19 15:31:40 +0300

    〘𝐶𝛩𝑁𝐴𝑁╎👑╎𝐵𝛩𝑇〙

--- plugins/setpp.js
+++ plugins/setpp.js
@@ -39,7 +39,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 
 handler.help = ['setppbotfull']
 handler.tags = ['owner']
-handler.command = ['بروفايل-البوت']
+handler.command = ['تغيير-البروفايل']
 
 handler.owner = true
 
