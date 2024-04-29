afdefc7b7718c78d9fd4df139fd42dc43c2bbf03
Shinichi <133603446+maykel2007@users.noreply.github.com>
2024-04-29 14:07:34 +0300

    x

--- main.js
+++ main.js
@@ -545,7 +545,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `*+905360504045 البوت شغال بواسطه‍ه المطور سينتشي :\n\n ${uptime}+*`;
+  const bio = `*+905360504045 البوت شغال بواسطه‍ه المطور سينتشي :*\n\n*${uptime}+*`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
